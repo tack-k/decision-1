4da4dfe42114a34a95efb4538d4fe51f1452c6f8
tack-k <tyool9c@gmail.com>
2020-11-26 18:57:29 +0900

    データセット

---
+++ src/dataset.js
@@ -0,0 +1,16 @@
+const dataset = {
+eatingOut: {
+content: "ラーメン",
+content: "焼き肉",
+content: "寿司",
+content: "パスタ",
+content: "ピザ"
+},
+dinner: {
+  content: "肉じゃが",
+  content: "ハンバーグ",
+  content: "カレーライス",
+  content: "餃子",
+  content: "鍋"
+}
+} 
