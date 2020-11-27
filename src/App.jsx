54f0b9c60b9e3a4da7d38c4eb5367d6add0b578e
tack-k <tyool9c@gmail.com>
2020-11-27 20:02:57 +0900

    データのランダム取得

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/styles/style.css';
+import React, { useState } from 'react';
+import Dataset from './dataset';
+import { ButtonSubmit, Dinner } from './component/index';
 
 function App() {
+
+  const [dataset, setDataset] = useState(Dataset);
+  const [dinner, setDinner] = useState("今日の夕食");
+
+  const selectDinner = () => {
+    const menu = dataset.dinner
+    const random = Math.floor(Math.random() * (menu.length));
+    const dinnerName = menu[random].content
+    setDinner(dinnerName);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div className="bg">
+    <section className="inner">
+        <Dinner dinner={dinner} />
+        <ButtonSubmit selectDinner={selectDinner} buttonName="決定"/> 
+    </section>
     </div>
   );
 }
